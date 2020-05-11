61757da67e2e586e3407f10010189f3f11ded3b5
CJHydraGenZ <cahyasaga321@gmail.com>
2020-05-12 00:34:53 +0800

    Updates

---
+++ precache-manifest.3cd57596367ae5c1c1dd85fbb5361c0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33aa3079d844234422c42a9df26d3466",
+    "url": "/CoronaVirus/index.html"
+  },
+  {
+    "revision": "48e8c32c21b4b80962e1",
+    "url": "/CoronaVirus/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8aa7f53d65667d8cd32a",
+    "url": "/CoronaVirus/static/js/2.0d7e9c95.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/CoronaVirus/static/js/2.0d7e9c95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48e8c32c21b4b80962e1",
+    "url": "/CoronaVirus/static/js/main.fbe1de67.chunk.js"
+  },
+  {
+    "revision": "f02187752a796a4328cb",
+    "url": "/CoronaVirus/static/js/runtime-main.a6924640.js"
+  }
+]);
